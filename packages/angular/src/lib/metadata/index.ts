dee093caf0bbed415fe69116e1b5264b39cbbd49
Igor Katsuba <igor@katsuba.dev>
2024-09-07 20:29:19 +0300

    fix(angular): add missed the `removeMetadataProperty` export

--- packages/angular/src/lib/metadata/index.ts
+++ packages/angular/src/lib/metadata/index.ts
@@ -3,4 +3,5 @@ export * from './get-metadata';
 export * from './get-metadata-property';
 export * from './standalone';
 export * from './push-to-metadata-property';
+export * from './remove-metadata-property';
 export * from './set-metadata-property';
